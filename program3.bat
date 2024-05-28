12c0ce79f92c25a094d57fbc2409822de71876f4
Alkon03 <kuimoff.lescha@yandex.ru>
2024-05-28 23:36:13 +0300

    first commit

---
+++ program3.bat
@@ -0,0 +1,22 @@
+echo off
+md Kuimov
+cd Kuimov
+md Aleksei
+md Kuimov
+pause
+cd Aleksei
+echo > Asus14.txt
+cd..
+cd Kuimov
+echo > 03032001.txt
+cd..
+cd..
+pause
+del Kuimov /S /Q /F
+pause
+cd Kuimov
+rd Kuimov
+rd Aleksei
+cd..
+rd Kuimov
+pause
